385de9a86c1ea84462bad5cb5ffa3caa4c90a5e5
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-23 23:26:12 +0530

    service layer setup for wikis
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/domain/repository/WikiRepository.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/domain/repository/WikiRepository.kt
@@ -1,7 +1,7 @@
 package com.example.androidapplicationtemplate.domain.repository
 
-import com.example.androidapplicationtemplate.core.network.Resource
+import com.example.androidapplicationtemplate.data.models.response.Response
 
 interface WikiRepository {
-    suspend fun someCrudOperation(): Resource<String>
+    suspend fun someCrudOperation(): List<Response>
 }
