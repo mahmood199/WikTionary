1066c21445b87968f7daed9ea7911407cbfb1a40
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-23 23:25:55 +0530

    service layer setup for wikis
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/remote/remoteDataSource/WikiRemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.example.androidapplicationtemplate.data.remote.remoteDataSource
+
+import com.example.androidapplicationtemplate.data.models.request.Request
+import com.example.androidapplicationtemplate.data.models.response.Response
+import com.example.androidapplicationtemplate.data.remote.remoteServices.WikiService
+import javax.inject.Inject
+
+class WikiRemoteDataSource @Inject constructor(
+	private val wikiService: WikiService
+) {
+
+	suspend fun get(): List<Response> {
+		return wikiService.getWiki(Request(1))
+	}
+
+}
