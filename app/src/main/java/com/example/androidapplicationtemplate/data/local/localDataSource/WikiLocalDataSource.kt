0c3f5fdd0228b21ff380dba7724296ce6a4238aa
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-23 20:54:49 +0530

    Added WikiLocalDataSource.kt abstract class.
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ app/src/main/java/com/example/androidapplicationtemplate/data/local/localDataSource/WikiLocalDataSource.kt
@@ -0,0 +1,9 @@
+package com.example.androidapplicationtemplate.data.local.localDataSource
+
+import com.example.androidapplicationtemplate.data.local.dao.WikiDao
+import javax.inject.Inject
+
+class WikiLocalDataSource @Inject constructor(wikiDao: WikiDao) {
+
+
+}
