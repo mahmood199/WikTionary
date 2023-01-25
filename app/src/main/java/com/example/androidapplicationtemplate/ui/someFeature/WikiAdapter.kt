8d4f67028282f90440ffd88b1669919974c9af3b
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-25 14:10:58 +0530

    Addition of new adapter
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

---
+++ app/src/main/java/com/example/androidapplicationtemplate/ui/someFeature/WikiAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.androidapplicationtemplate.ui.someFeature
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.androidapplicationtemplate.R
+import com.example.androidapplicationtemplate.data.models.response.Page
+import com.example.androidapplicationtemplate.databinding.ItemWikiBinding
+
+class WikiAdapter(
+    private val list: MutableList<Page>,
+) : RecyclerView.Adapter<WikiAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemWikiBinding.inflate(
+            LayoutInflater.from(parent.context)
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindData(list[position])
+    }
+
+    override fun getItemCount() = list.size
+
+
+    inner class ViewHolder(private val binding: ItemWikiBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindData(page: Page) {
+            binding.apply {
+                tvField1.text = page.title
+                tvField2.text = page.pageId.toString()
+                tvField3.text = page.index.toString()
+
+
+                Glide.with(root)
+                    .load(page.thumbnail?.source ?: "")
+                    .placeholder(ContextCompat.getDrawable(root.context, R.drawable.place_holder))
+                    .into(ivWiki)
+            }
+        }
+
+    }
+}
