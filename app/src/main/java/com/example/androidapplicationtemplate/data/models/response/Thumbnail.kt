569069b044b9a9878787103edc5af9c3c346a373
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-25 14:10:38 +0530

    Changes in model. some fields are nullable in response
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Thumbnail.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Thumbnail.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class Thumbnail(
     @SerializedName("height") val height: Int,
-    @SerializedName("source") val source: String,
+    @SerializedName("source") val source: String? = null,
     @SerializedName("width") val width: Int,
 )
