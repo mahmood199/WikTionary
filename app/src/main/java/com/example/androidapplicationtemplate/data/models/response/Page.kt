dd385ac46aa40d33b8439465e064ed6179f8288e
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-26 17:12:17 +0530

    data changes
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Page.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Page.kt
@@ -1,12 +1,19 @@
 package com.example.androidapplicationtemplate.data.models.response
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class Page(
+@Entity(tableName = "pages")
+class Page(
+    @PrimaryKey(autoGenerate = true)
+    @SerializedName("pageid") val pageId: Int,
     @SerializedName("index") val index: Int,
     @SerializedName("ns") val ns: Int,
-    @SerializedName("pageid") val pageId: Int,
-    @SerializedName("terms") val terms: Terms,
-    @SerializedName("thumbnail") val thumbnail: Thumbnail? = null,
     @SerializedName("title") val title: String,
-)
+) {
+    @Ignore
+    @SerializedName("thumbnail") val thumbnail: Thumbnail? = null
+
+}
