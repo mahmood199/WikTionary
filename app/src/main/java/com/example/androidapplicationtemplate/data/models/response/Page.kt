0dc7d015cb489af51dc968ebc2caea895e90c965
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-26 19:20:19 +0530

    page modified to redirect to new activity. now it contains parcelable implementation
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Page.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/models/response/Page.kt
@@ -1,10 +1,13 @@
 package com.example.androidapplicationtemplate.data.models.response
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "pages")
 class Page(
     @PrimaryKey(autoGenerate = true)
@@ -12,7 +15,7 @@ class Page(
     @SerializedName("index") val index: Int,
     @SerializedName("ns") val ns: Int,
     @SerializedName("title") val title: String,
-) {
+) : Parcelable {
     @Ignore
     @SerializedName("thumbnail") val thumbnail: Thumbnail? = null
 
