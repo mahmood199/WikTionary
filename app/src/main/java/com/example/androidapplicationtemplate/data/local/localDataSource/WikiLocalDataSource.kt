95677c8c3f8180624910587537667a242dbc01e3
MSI KATANA <mohammed.mahmood@semaai.com>
2023-01-26 17:12:51 +0530

    data layer changes
    Signed-off-by: MSI KATANA <mohammed.mahmood@semaai.com>

--- app/src/main/java/com/example/androidapplicationtemplate/data/local/localDataSource/WikiLocalDataSource.kt
+++ app/src/main/java/com/example/androidapplicationtemplate/data/local/localDataSource/WikiLocalDataSource.kt
@@ -1,12 +1,18 @@
 package com.example.androidapplicationtemplate.data.local.localDataSource
 
 import com.example.androidapplicationtemplate.data.local.dao.WikiDao
+import com.example.androidapplicationtemplate.data.models.response.Page
+import com.example.androidapplicationtemplate.data.models.response.Response
 import javax.inject.Inject
 
-class WikiLocalDataSource @Inject constructor(wikiDao: WikiDao) {
+class WikiLocalDataSource @Inject constructor(val wikiDao: WikiDao) {
 
-    suspend fun addAll() {
+    suspend fun addAll(result: Response): LongArray {
+        return wikiDao.insertAllWikis(result.query.pages)
+    }
 
+    suspend fun getAll(searchQuery: String) : List<Page> {
+        return wikiDao.getWikis(searchQuery)
     }
 
 }
